43bf25bf1a18240adeaac1098dbbf44ddab20628
Bekkhan <israpal@bk.ru>
2024-09-27 11:25:12 +0300

    fixed postgres connection

--- twitter_clone/config.py
+++ twitter_clone/config.py
@@ -4,35 +4,12 @@ from fastapi import status
 
 from schemas.error import ErrorResult
 
-POSTGRES_USER = os.getenv("POSTGRES_USER")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT")
-POSTGRES_DB = os.getenv("DB_NAME")
-DEMO_MODE = os.getenv("DEMO_MODE")
+POSTGRES_USER = os.getenv("POSTGRES_USER", "admin")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "admin")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+POSTGRES_DB = os.getenv("POSTGRES_DB", "twitter_db")
+DEMO_MODE = os.getenv("DEMO_MODE", "false").lower() == "true"
 
-if POSTGRES_USER is None:
-    POSTGRES_USER = "admin"
-
-if POSTGRES_PASSWORD is None:
-    POSTGRES_PASSWORD = "admin"
-
-if POSTGRES_HOST is None:
-    POSTGRES_HOST = "localhost"
-
-if POSTGRES_PORT is None:
-    POSTGRES_PORT = "5432"
-
-if POSTGRES_DB is None:
-    POSTGRES_DB = "twitter_db"
-
-if DEMO_MODE is None:
-    DEMO_MODE = False
-else:
-    if DEMO_MODE.lower() == "true":
-        DEMO_MODE = True
-    else:
-        DEMO_MODE = False
 
 MEDIA_FILE_NAME = "{image_id}.jpg"
 GET_FOX_URL = "https://randomfox.ca/images/{image_id}.jpg"
--- twitter_clone/database.py
+++ twitter_clone/database.py
@@ -3,19 +3,18 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 from config import (
     POSTGRES_DB,
-    POSTGRES_HOST,
     POSTGRES_PASSWORD,
     POSTGRES_PORT,
     POSTGRES_USER,
 )
 
-POSTGRES_URL = "postgresql+asyncpg://{user}:{password}@{host}:{port}/{db_name}".format(
+POSTGRES_URL = "postgresql+asyncpg://{user}:{password}@twitter_db:{port}/{db_name}".format(
     user=POSTGRES_USER,
     password=POSTGRES_PASSWORD,
-    host=POSTGRES_HOST,
     port=POSTGRES_PORT,
     db_name=POSTGRES_DB,
 )
+
 engine = create_async_engine(POSTGRES_URL)  # echo=True)
 
 AsyncSessionLocal = sessionmaker(
